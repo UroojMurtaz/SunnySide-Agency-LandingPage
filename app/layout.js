5013c4625349f09c1be1149a709cab16d2129202
UroojMurtaz <uroojmurtaza12@gmail.com>
2023-08-28 16:47:48 +0500

    check deploy errors

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <main>{children}</main>
-     
-    </>
-
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   )
 }
